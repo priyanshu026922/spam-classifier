23b0dc76042d30e52e9d41a79e485ee9c12aeed6
priyanshu026922 <btech10886.23@bitmesra.ac.in>
2025-10-19 21:16:40 +0530

    updated file

--- app.py
+++ app.py
@@ -43,15 +43,17 @@ st.title("EMAIL SPAM OR NOT SPAM CLASSIFIER")
 input_sms=st.text_input("Enter the message")
 
 if st.button('Predict'):
-    tranfomed_sms=transform_text(input_sms)
-    vector_input=tfidf.transform([tranfomed_sms])
-    res=model.predict(vector_input)[0]
-
-
-    if res==1:
-        st.header("Spam")
+    if input_sms.strip() == "":
+        st.warning("Please enter a message first.")
     else:
-        st.header("Not Spam")
+        tranfomed_sms = transform_text(input_sms)
+        vector_input = tfidf.transform([tranfomed_sms])
+        res = model.predict(vector_input)[0]
+
+        if res == 1:
+            st.header("🚨 Spam")
+        else:
+            st.header("✅ Not Spam")
 #1.prerprocess
 #2.vectorize
 #3.predict
